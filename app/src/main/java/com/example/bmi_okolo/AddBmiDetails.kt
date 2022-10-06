8867d3b57ed7376c94eb60f4df781e302fd86173
Arthur Okolo <Okoloarthur4@gmail.com>
2022-10-06 22:02:26 +0100

    Body-Mass-Index calculator for users

---
+++ app/src/main/java/com/example/bmi_okolo/AddBmiDetails.kt
@@ -0,0 +1,43 @@
+package com.example.bmi_okolo
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.bmi_okolo.databinding.FragmentAddBmiDetailsBinding
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class AddBmiDetails : Fragment() {
+
+    private var _binding: FragmentAddBmiDetailsBinding? = null
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentAddBmiDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.calculate.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
